fcc3c95733ca8c96c1dd6115cdb3ffd1f5fd8c57
pengshi2 <boogiesnoogawoot@gmail.com>
2023-02-09 10:16:22 -0500

    c

--- create-task/main.js
+++ create-task/main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+let computerScore = 1;
+
+let playerScore = 1;
+
+const randomChoice = [rock, paper, scissors];
+
+const rock = buttons - rock;
+const paper = buttons - paper;
+const scissors = buttons - scissors;
+let displayDiv = document.getElementById(".display");
+
+let randomNum = Math.floor(Math.random() * randomChoice.length);
+console.log(randomNum);
+
+if (randomNum === 0 || randomNum === 1) {
+  console.log("rock");
+} else console.log("paper");
