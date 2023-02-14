fa1d424a09265e1bfac849b967212c4f5ff51296
pengshi2 <boogiesnoogawoot@gmail.com>
2023-02-14 10:01:52 -0500

    a

--- create-task/main.js
+++ create-task/main.js
@@ -4,12 +4,31 @@ let computerScore = 1;
 
 let playerScore = 1;
 
+const rock = document.getElementById("buttons-rock");
+const paper = document.getElementById("buttonspaper");
+const scissors = document.getElementById("buttonsscissors");
+
+const icons = [
+  {
+    name: "rock",
+    image:
+      "https://www.seekpng.com/png/detail/111-1113492_rock-paper-scissors-boston.png",
+  },
+
+  {
+    name: "paper",
+    image: "https://nehalhazem.github.io/rockPaperScissors.io/img/paper.png",
+  },
+
+  {
+    name: "scissors",
+    image: "https://nehalhazem.github.io/rockPaperScissors.io/img/scissors.png",
+  },
+];
+
 const randomChoice = [rock, paper, scissors];
 
-const rock = buttons - rock;
-const paper = buttons - paper;
-const scissors = buttons - scissors;
-let displayDiv = document.getElementById(".display");
+let displayDiv = document.querySelector("#display");
 
 let randomNum = Math.floor(Math.random() * randomChoice.length);
 console.log(randomNum);
@@ -17,3 +36,14 @@ console.log(randomNum);
 if (randomNum === 0 || randomNum === 1) {
   console.log("rock");
 } else console.log("paper");
+
+function display() {
+  icons.forEach((image) => {
+    displayDiv.insertAdjacentHTML(
+      "beforeend",
+      `<img class = "icons" src = "${icons.image}">  `
+    );
+  });
+}
+
+display();
