cb81d7be5173284ed0834bc9bd07099d64225062
pengshi2 <boogiesnoogawoot@gmail.com>
2023-02-15 10:11:22 -0500

    a

---
+++ create-task/icons.js
@@ -0,0 +1,16 @@
+export const icons = [
+  {
+    name: "rock",
+    image: "https://nehalhazem.github.io/rockPaperScissors.io/img/rock.png",
+  },
+
+  {
+    name: "paper",
+    image: "https://nehalhazem.github.io/rockPaperScissors.io/img/paper.png",
+  },
+
+  {
+    name: "scissors",
+    image: "https://nehalhazem.github.io/rockPaperScissors.io/img/scissors.png",
+  },
+];
--- create-task/main.js
+++ create-task/main.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import { icons } from "./icons.js";
 
 let computerScore = 1;
 
@@ -8,24 +9,6 @@ const rock = document.getElementById("buttons-rock");
 const paper = document.getElementById("buttonspaper");
 const scissors = document.getElementById("buttonsscissors");
 
-const icons = [
-  {
-    name: "rock",
-    image:
-      "https://www.seekpng.com/png/detail/111-1113492_rock-paper-scissors-boston.png",
-  },
-
-  {
-    name: "paper",
-    image: "https://nehalhazem.github.io/rockPaperScissors.io/img/paper.png",
-  },
-
-  {
-    name: "scissors",
-    image: "https://nehalhazem.github.io/rockPaperScissors.io/img/scissors.png",
-  },
-];
-
 const randomChoice = [rock, paper, scissors];
 
 let displayDiv = document.querySelector("#display");
@@ -33,15 +16,19 @@ let displayDiv = document.querySelector("#display");
 let randomNum = Math.floor(Math.random() * randomChoice.length);
 console.log(randomNum);
 
-if (randomNum === 0 || randomNum === 1) {
+if (randomNum === 0) {
   console.log("rock");
-} else console.log("paper");
+} else if (randomNum === 1) {
+  console.log("paper");
+} else if (randomNum === 2) {
+  console.log("scissors");
+}
 
-function display() {
+function display(array) {
   icons.forEach((image) => {
     displayDiv.insertAdjacentHTML(
       "beforeend",
-      `<img class = "icons" src = "${icons.image}">  `
+      `<img class = "icon" src = "${image.image}"> `
     );
   });
 }
