8f1fcb51ae7b145aa508327e111ca32a9aba322c
pengshi2 <pengshi0921@gmail.com>
2023-03-01 14:18:29 -0500

    s

--- create-task/main.js
+++ create-task/main.js
@@ -1,87 +1,3 @@
-// import "./style.css";
-// import { Timothy } from "./assets.js";
-// import { you } from "./assets.js";
-
-// const computerScore = 0;
-// const playerScore = 0;
-
-// const rock = document.getElementById("rock");
-
-// const paper = document.getElementById("paper");
-
-// const scissors = document.getElementById("scissors");
-
-// const playerScoreboard = document.querySelector("p-score");
-
-// const playerChoice = [rock, paper, scissors];
-
-// const computerScoreboard = document.querySelector("c-score");
-
-// const randomChoice = [rock, paper, scissors];
-
-// const result = document.querySelector(".result");
-// let displayDiv = document.querySelector("#display");
-
-// playerChoice.forEach((option) => {
-//   option.addEventListener("click", function () {
-//     const movesLeft = document.querySelector(".movesleft");
-//     movesLeft++;
-//     movesLeft.innerText = `Moves Left: ${10 - moves}`;
-//     let randomNum = Math.floor(Math.random() * randomChoice.length);
-//     if (randomNum === 0) {
-//       let computerChoice = "rock";
-//     } else if (randomNum === 1) {
-//       let computerChoice = "paper";
-//     } else if (randomNum === 2) {
-//       let computerChoice = "scissors";
-//     }
-//   });
-// });
-// console.log(randomNum);
-
-// console.log(computerChoice);
-
-// function displayTimothy() {
-//   Timothy.forEach((image) => {
-//     displayDiv.insertAdjacentHTML(
-//       "beforeend",
-//       `<h2 class = "name"> ${image.name}</h2>
-//       <img class = "icon" src = "${image.image}"> `
-//     );
-//   });
-// }
-
-// function displayPlayer() {
-//   you.forEach((image) => {
-//     displayDiv.insertAdjacentHTML(
-//       "beforeend",
-//       `<h2 class = "name"> ${image.name}</h2>
-//       <img class = "icon" src = "${image.image}"> `
-//     );
-//   });
-// }
-
-// displayPlayer();
-// displayTimothy();
-
-// document.getElementById("rock").addEventListener("click", function () {
-//   displayDiv.insertAdjacentHTML("beforeend", `<h3 class = "test> test </h3>`);
-// });
-
-// if (playerChoice === computerChoice) {
-//   result.textContent = "Tie";
-// } else if (playerChoice === "Rock") {
-//   if (computerChoice === "Paper") {
-//     result.textContent = "Timothy Won";
-//     computerScore++;
-//     computerScoreboard.textContent = computerScore;
-//   }
-// } else {
-//   result.textContent = "You Won";
-//   playerScore++;
-//   playerScoreboard.textContent = playerScore;
-// }
-
 const game = () => {
   let playerScore = 0;
   let computerScore = 0;
@@ -92,7 +8,7 @@ const game = () => {
     const paper = document.querySelector(".paper");
     const scissor = document.querySelector(".scissor");
     const playerChoices = [rock, paper, scissor];
-    const computerOptions = ["rock", "paper", "scissors"];
+    const computerChoices = ["rock", "paper", "scissors"];
 
     playerChoices.forEach((option) => {
       option.addEventListener("click", function () {
@@ -119,36 +35,36 @@ const game = () => {
     player = player.toLowerCase();
     computer = computer.toLowerCase();
     if (player === computer) {
-      result.textContent = "Tie";
+      result.innerHTML = "Tie";
     } else if (player === "rock") {
       if (computer === "paper") {
-        result.textContent = "Computer Won";
+        result.innerHTML = "Computer Won";
         computerScore++;
-        computerScoreBoard.textContent = computerScore;
+        computerScoreBoard.innerHTML = computerScore;
       } else {
-        result.textContent = "Player Won";
+        result.innerHTML = "Player Won";
         playerScore++;
-        playerScoreBoard.textContent = playerScore;
+        playerScoreBoard.innerHTML = playerScore;
       }
     } else if (player === "scissors") {
       if (computer === "rock") {
         result.textContent = "Computer Won";
         computerScore++;
-        computerScoreBoard.textContent = computerScore;
+        computerScoreBoard.innerHTML = computerScore;
       } else {
-        result.textContent = "Player Won";
+        result.innerHTML = "Player Won";
         playerScore++;
-        playerScoreBoard.textContent = playerScore;
+        playerScoreBoard.innerHTML = playerScore;
       }
     } else if (player === "paper") {
       if (computer === "scissors") {
-        result.textContent = "Computer Won";
+        result.innerHTML = "Computer Won";
         computerScore++;
-        computerScoreBoard.textContent = computerScore;
+        computerScoreBoard.innerHTML = computerScore;
       } else {
-        result.textContent = "Player Won";
+        result.innerHTML = "Player Won";
         playerScore++;
-        playerScoreBoard.textContent = playerScore;
+        playerScoreBoard.innerHTML = playerScore;
       }
     }
   };
