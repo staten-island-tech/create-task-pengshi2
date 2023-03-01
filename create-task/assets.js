42751c0ea41fcc6c9349ba3ae9723e8df16643c3
pengshi2 <pengshi0921@gmail.com>
2023-03-01 14:13:37 -0500

    Update assets.js

--- create-task/assets.js
+++ create-task/assets.js
@@ -1,20 +1,3 @@
-// export const icons = [
-//   {
-//     name: "rock",
-//     image: "https://nehalhazem.github.io/rockPaperScissors.io/img/rock.png",
-//   },
-
-//   {
-//     name: "paper",
-//     image: "https://nehalhazem.github.io/rockPaperScissors.io/img/paper.png",
-//   },
-
-//   {
-//     name: "scissors",
-//     image: "https://nehalhazem.github.io/rockPaperScissors.io/img/scissors.png",
-//   },
-// ];
-
 export const Timothy = [
   {
     name: "Timothy the Robot",
