adb211721f536e5c87e49ca846f01fa0638c9d9a
pengshi2 <pengshi0921@gmail.com>
2023-03-01 17:43:05 -0500

    s

--- create-task/assets.js
+++ create-task/assets.js
@@ -4,11 +4,3 @@ export const Timothy = [
     image: "https://i1.sndcdn.com/artworks-000623298505-jqoikf-t500x500.jpg",
   },
 ];
-
-export const you = [
-  {
-    name: "You",
-    image:
-      "https://i.pinimg.com/736x/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg",
-  },
-];
--- create-task/main.js
+++ create-task/main.js
@@ -3,7 +3,7 @@ const game = () => {
   let computerScore = 0;
   let moves = 0;
 
-  const playGame = () => {
+  const initialize = () => {
     const rock = document.querySelector(".rock");
     const paper = document.querySelector(".paper");
     const scissor = document.querySelector(".scissor");
@@ -13,16 +13,19 @@ const game = () => {
     playerChoices.forEach((option) => {
       option.addEventListener("click", function () {
         const movesLeft = document.querySelector(".movesleft");
+
         moves++;
+
         movesLeft.innerText = `Moves Left: ${10 - moves}`;
 
         const choiceNumber = Math.floor(Math.random() * 3);
+
         const computerChoice = computerChoices[choiceNumber];
 
         winner(this.innerText, computerChoice);
 
         if (moves === 10) {
-          gameOver(playerChoices, movesLeft);
+          terminate(playerChoices, movesLeft);
         }
       });
     });
@@ -39,37 +42,49 @@ const game = () => {
     } else if (player === "rock") {
       if (computer === "paper") {
         result.innerHTML = "Computer Won";
+
         computerScore++;
+
         computerScoreBoard.innerHTML = computerScore;
       } else {
         result.innerHTML = "Player Won";
+
         playerScore++;
+
         playerScoreBoard.innerHTML = playerScore;
       }
     } else if (player === "scissors") {
       if (computer === "rock") {
         result.textContent = "Computer Won";
+
         computerScore++;
+
         computerScoreBoard.innerHTML = computerScore;
       } else {
         result.innerHTML = "Player Won";
+
         playerScore++;
+
         playerScoreBoard.innerHTML = playerScore;
       }
     } else if (player === "paper") {
       if (computer === "scissors") {
         result.innerHTML = "Computer Won";
+
         computerScore++;
+
         computerScoreBoard.innerHTML = computerScore;
       } else {
         result.innerHTML = "Player Won";
+
         playerScore++;
+
         playerScoreBoard.innerHTML = playerScore;
       }
     }
   };
 
-  const gameOver = (playerChoices, movesLeft) => {
+  const terminate = (playerChoices, movesLeft) => {
     const chooseMove = document.querySelector(".move");
     const result = document.querySelector(".result");
     const reloadBtn = document.querySelector(".reload");
@@ -83,28 +98,38 @@ const game = () => {
 
     if (playerScore > computerScore) {
       result.style.fontSize = "2rem";
+
       result.innerText = "You beat Timothy!";
+
       result.style.color = "green";
     } else if (playerScore < computerScore) {
       result.style.fontSize = "2rem";
+
       result.innerText = "Timothy beat you!";
+
       result.style.color = "red";
     } else {
       result.style.fontSize = "2rem";
-      result.innerText = "Tie!";
+
+      result.innerText = "Draw!";
+
       result.style.color = "grey";
     }
     reloadBtn.innerText = "Restart?";
-    reloadBtn.style.display = "flex";
+
     reloadBtn.addEventListener("click", () => {
       window.location.reload();
     });
   };
 
-  const history = [];
-
-  document.querySelector("#history");
-  playGame();
+  initialize();
 };
 
 game();
+
+const history = [];
+
+const historyList = document.querySelector("#history");
+for (let i = 0; i < historyList.length; i++) {
+  historyList.innerHTML = `${result}`;
+}
